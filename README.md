1d1f81eecc210773e42e3940dd1d9d7b6d228565
Trunkmonky <drew.w.schmidt@gmail.com>
2023-02-17 14:02:17 -0800

    Add files

---
+++ README.md
@@ -0,0 +1,1 @@
+# THE To Do App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import _ from 'lodash';
+
+const test = 'Hello World';
+
+console.log(test);
