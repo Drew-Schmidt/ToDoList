819b94dded9b6a33463013a66d600ba9717664f5
Trunkmonky <drew.w.schmidt@gmail.com>
2023-02-19 06:40:08 -0800

    Add: Log data-values

--- src/index.js
+++ src/index.js
@@ -1,8 +1,46 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import _ from 'lodash';
+import _, { forEach } from 'lodash';
+import * as dynamicElements from './DOMfunctions';
+import * as controlFunction from './Controllers';
 
-const test1 = 'Hello World';
+// Query static buttons
+const submitList = document.querySelector('#submitList');
+const submitTask = document.querySelector('#submitTask');
+const addList = document.querySelector('#newList');
+const addTask = document.querySelector('#newTask');
 
-console.log(test1);
+// Create New list/project
+submitList.addEventListener('click', () => {
+  // Create elements from form value
+  dynamicElements.addList(addList);
+  // Save form data to myList array
+  controlFunction.createListObject();
+});
+
+// Create New task
+submitTask.addEventListener('click', () => {
+  // Create elements from form values
+  dynamicElements.addTaskElements(addTask.value);
+  // Save form data to list object
+  controlFunction.createTaskObject();
+  // Mark task completed
+  controlFunction.completeTask();
+  // Remove task
+  controlFunction.removeTask();
+  // Edit task
+});
+
+// Test task population from lists within myLists
+// Double forEach() for 'all to-do items' at window load
+controlFunction.myList.forEach((list) => {
+  list.task.forEach((task) => {
+    // Create elements from list objects
+    dynamicElements.addTaskElements(task.notes);
+    // Mark task Completed
+    controlFunction.completeTask();
+    // Remove task
+    controlFunction.removeTask();
+  });
+});
